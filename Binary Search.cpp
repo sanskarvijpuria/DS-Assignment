c56c9de3d11228b7b7cda4c8f668d562dc10e3c8
Sanskar Vijpuria <54209147+sanskarvijpuria@users.noreply.github.com>
2019-11-18 21:05:17 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -5,10 +5,40 @@ using namespace std;
 
 int main()
 {
-    int ;
+    int n,avg,e,count=0;
+    cout<<"Enter the number of elements in array"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Enter the sorted array"<<endl;
+    for(int i=0;i<n;i++)
+            {
+                cin>>a[i];
+            }
 
-    cout<<"";
-    cin>>;
+    cout<<"Element to be searched"<<endl;
+    cin>>e;
+    avg=(n-1)/2;
+
+    while(count==0)
+        {
+            if(a[avg]==e)
+                {
+                   cout<<"Element found at:"<<avg+1<<endl;
+                   count++;
+                }
+            else if(e<a[avg])
+                {
+                    avg=avg/2;
+                }
+            else if(e>a[avg])
+                {
+                   avg=(avg+n-1)/2;
+                }
+            else
+                {
+                    cout<<"Element not in array "<<endl;
+                }
+           }
 
     getch();
     return 0;
