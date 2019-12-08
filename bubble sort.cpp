c9de711e94bd0daa8efc96df856b6b1b73110a4b
Sanskar Vijpuria <54209147+sanskarvijpuria@users.noreply.github.com>
2019-12-08 23:57:24 +0530

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,46 +1,43 @@
-#include <iostream>
- 
+#include<iostream>
+#include<conio.h>
+
 using namespace std;
- 
-// Sort arr[] of size n using Bubble Sort. 
-void BubbleSort (int arr[], int n)
-{
-	int i, j;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = 0; j < n-i-1; ++j)
-		{
-			// Comparing consecutive data and switching values if value at j > j+1.
-			if (arr[j] > arr[j+1])
-			{
-				arr[j] = arr[j]+arr[j+1];
-				arr[j+1] = arr[j]-arr[j + 1];
-				arr[j] = arr[j]-arr[j + 1];
-			}
-		}
-		// Value at n-i-1 will be maximum of all the values below this index.
-	}	
-}	
- 
+
 int main()
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
- 
-	int arr[n];
-	for(i = 0; i < n; i++)
-	{
-		cout<<"Enter element "<<i+1<<": ";
-		cin>>arr[i];
-	}
- 
-	BubbleSort(arr, n);
- 
-	// Display the sorted data.
-	cout<<"\nSorted Data ";
-	for (i = 0; i < n; i++)
-        cout<<"->"<<arr[i];
- 
-	return 0;
+    int i,n,count=0;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Enter the elements"<<endl;
+    void swap(int *a, int *b);
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    while(count<n)
+        {
+          for(i=0;i<n;i++)
+            {
+               if(a[i]>a[i+1])
+               {
+                   swap(&a[i],&a[i+1]);
+               }
+            }
+            count++;
+        }
+   cout<<"here the final array\n";
+   for(i=0;i<n;i++)
+     {
+       cout<<a[i]<<endl;
+     }
+    getch();
+    return 0;
 }
+
+void swap(int *a, int *b)
+        {
+            int c;
+            c=*a;
+            *a=*b;
+            *b=c;
+        }
+
